e4b1f7fcec87a8c22d981b875dc56bac5e4cd113
Ryan Rickgauer <rrickgauer1@gmail.com>
2019-12-07 16:51:50 -0600

    added sort table function

--- index.php
+++ index.php
@@ -438,9 +438,7 @@
 			<!-- completed citations section -->
 			<div class="col-md-6 col-sm-12">
 
-
-
-				<table class="table table-sm">
+				<table class="table table-sm" id="myTable">
 
 					<thead>
 						<tr>
@@ -449,10 +447,7 @@
 						</tr>
 					</thead>
 					<tbody id="completed-section">
-
 					</tbody>
-
-
 
 				</table>
 
@@ -467,16 +462,54 @@
 	</div>
 
 	<script>
-
 		function addCitationRow(citation) {
 			var citationCell = '<td>' + citation + '</td>';
 			var trash = '<td><ion-icon name="trash" class="remove-row-button" onclick="removeCitationRow(this)"></ion-icon></td>';
 			$("#completed-section").append('<tr>' + citationCell + trash + '</tr>');
+
+			sortTable();
+
+			// sortTable($('#tblNodes'),'desc');
 		}
 
 		function removeCitationRow(citation) {
 			if (confirm('Delete citation?')) {
 				$(citation).closest("tr").remove();
+			}
+		}
+
+		function sortTable() {
+			var table, rows, switching, i, x, y, shouldSwitch;
+			table = document.getElementById("myTable");
+			switching = true;
+			/*Make a loop that will continue until
+			no switching has been done:*/
+			while (switching) {
+				//start by saying: no switching is done:
+				switching = false;
+				rows = table.rows;
+				/*Loop through all table rows (except the
+				first, which contains table headers):*/
+				for (i = 1; i < (rows.length - 1); i++) {
+					//start by saying there should be no switching:
+					shouldSwitch = false;
+					/*Get the two elements you want to compare,
+					one from current row and one from the next:*/
+					x = rows[i].getElementsByTagName("TD")[0];
+					y = rows[i + 1].getElementsByTagName("TD")[0];
+					//check if the two rows should switch place:
+					if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+						//if so, mark as a switch and break the loop:
+						shouldSwitch = true;
+						break;
+					}
+				}
+				if (shouldSwitch) {
+					/*If a switch has been marked, make the switch
+					and mark that a switch has been done:*/
+					rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+					switching = true;
+				}
 			}
 		}
 
@@ -513,7 +546,7 @@
 			var publisher = getBookPublisher();
 			var publicationDate = getBookPublicationDate();
 
-			var citation =  author + title + publisher + publicationDate;
+			var citation = author + title + publisher + publicationDate;
 			// $("#completed-section").append(citation);
 
 			addCitationRow(citation);
@@ -932,16 +965,6 @@
 				return '';
 			}
 		}
-
-
-
-
-
-
-
-
-
-
 	</script>
 
 
